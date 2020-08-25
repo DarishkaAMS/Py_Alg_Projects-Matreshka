d8a7ba34541f42721a147fb11efd3c16fd7a4bfd
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-08-25 06:34:12 +0300

    Matreshka is added

---
+++ Matreshka.py
@@ -0,0 +1,13 @@
+counter = 0
+def matreshka(n):
+    # print("I'm Matreshka" + str(n), "I was born")
+
+    if n == 1:
+        print("Matreshka")
+    else:
+        print("Upper Part = ", n)
+        matreshka(n-1)
+        print("Lower Part = ", n)
+    # print("I'm Matreshka" + str(n), "I died")
+
+matreshka(5)
